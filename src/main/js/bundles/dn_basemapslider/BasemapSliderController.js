33c6a7dead09926d2935903f01420a6445d59777
Matthias Stein <m.stein@conterra.de>
2021-06-07 08:15:32 +0200

    Removed unused import in BasemapSliderController.js

--- src/main/js/bundles/dn_basemapslider/BasemapSliderController.js
+++ src/main/js/bundles/dn_basemapslider/BasemapSliderController.js
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import ct_async from "ct/async";
-
 export default class {
 
     activate() {
