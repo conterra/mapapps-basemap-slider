92db1d6ec134caef5764555b3e4c483108225945
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-11-17 14:51:07 +0100

    Refactor new interface as separate component

--- src/main/js/bundles/dn_basemapslider/template/BasemapSliderWidget.vue
+++ src/main/js/bundles/dn_basemapslider/template/BasemapSliderWidget.vue
@@ -17,45 +17,11 @@
 -->
 <template>
     <div class="basemap-slider__container">
-        <div class="basemap-slider__selection-section">
-            <div
-                v-for="(basemap, index) in basemaps"
-                :key="`basemap-container-${basemap.id}`"
-            >
-                <span
-                    v-if="index === basemaps.length - 1 && rightElipsesActive"
-                    :key="`right-elipses-${index}`"
-                    class="basemap-slider__elipses icon-more-h"
-                />
-                <v-chip
-                    v-if="filteredBasemaps.includes(basemap) && !basemap.active"
-                    :id="basemap.id"
-                    :key="`chip-${basemap.id}`"
-                    :class="{ primary: basemap.active }"
-                    label
-                    @click="goToLayer(basemap.value)"
-                >
-                    {{ basemap.title }}
-                </v-chip>
-                <v-select
-                    v-else-if="filteredBasemaps.includes(basemap) && basemap.active"
-                    :key="`select-${basemap.id}`"
-                    class="basemap-slider__basemap-select"
-                    :value="basemap.value"
-                    :items="basemaps"
-                    item-text="title"
-                    item-value="value"
-                    outline
-                    hide-details
-                    @change="goToLayer($event)"
-                />
-                <span
-                    v-if="index === 0 && leftElipsesActive"
-                    :key="`left-elipses-${index}`"
-                    class="basemap-slider__elipses icon-more-h"
-                />
-            </div>
-        </div>
+        <basemap-slider-elipsis-section
+            :basemaps="basemaps"
+            @chip:go-to-layer="goToLayer($event)"
+            @select:go-to-layer="goToLayer($event)"
+        />
         <v-flex
             xs12
             px-4
@@ -105,8 +71,12 @@
 </template>
 <script>
     import Bindable from "apprt-vue/mixins/Bindable";
+    import BasemapSliderElipsisSection from "./subcomponents/BasemapSliderElipsisSection.vue";
 
     export default {
+        components: {
+            "basemap-slider-elipsis-section": BasemapSliderElipsisSection
+        },
         mixins: [Bindable],
         props: {
             basemaps: {
@@ -129,26 +99,6 @@
         computed: {
             count() {
                 return this.basemaps.length;
-            },
-            filteredBasemaps() {
-                const activeBasemapIndex = this.basemaps.findIndex(basemap => basemap.active);
-                return this.basemaps.filter((_, index) =>
-                    [
-                        0,
-                        activeBasemapIndex - 1,
-                        activeBasemapIndex,
-                        activeBasemapIndex + 1,
-                        this.basemaps.length - 1
-                    ].includes(index)
-                );
-            },
-            leftElipsesActive() {
-                const activeBasemapIndex = this.basemaps.findIndex(basemap => basemap.active);
-                return activeBasemapIndex > 2;
-            },
-            rightElipsesActive() {
-                const activeBasemapIndex = this.basemaps.findIndex(basemap => basemap.active);
-                return activeBasemapIndex < this.basemaps.length - 3;
             }
         },
         watch: {
